cf1d2a8d6f4d5398a00b06d22039381d388b6efa
21130410 <21130410@st.hcmuaf.edu.vn>
2025-11-13 17:00:21 +0700

    initial FigureShop E-Commerce platform

---
+++ FigureShopServer/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(); //ui
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseWebAssemblyDebugging();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseAuthorization();
+app.UseBlazorFrameworkFiles();
+app.MapRazorPages();
+
+//Ánh xạ routes cho Razor Pages
+//Ví dụ: / Login, / About, / Contact
+app.MapControllers();
+//Ánh xạ routes cho MVC Controllers
+//Theo convention: /{ controller}/{ action}/{ id ?}
+//Ví dụ: / Home / Index, / Products / Details / 5
+app.MapFallbackToFile("index.html"); 
+//Khi không tìm thấy route phù hợp với các endpoint đã định nghĩa
+
+//Nó sẽ trả về file index.html thay vì lỗi 404
+
+//Cho phép client-side routing xử lý các route còn lại
+
+app.Run();
